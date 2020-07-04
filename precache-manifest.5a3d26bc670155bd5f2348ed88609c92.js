802a970547c18743b069d2da4a2c78ea4d8c6207
Gaurav Mendse <gauravmendse@Gauravs-MacBook-Pro.local>
2020-07-04 16:01:49 +0530

    Updates

---
+++ precache-manifest.5a3d26bc670155bd5f2348ed88609c92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b23ca77096322bafd4da5492659d3ea",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b8903a2fc0464240bd3f",
+    "url": "/monsters-rolodex/static/css/main.2bec443d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8903a2fc0464240bd3f",
+    "url": "/monsters-rolodex/static/js/main.4100d53d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
